22c92330c1399660cb8d71ec73d3ee493d581573
Curran McConnell <curran@atidot.com>
2020-03-09 21:24:20 -0400

    Separated main into functions

--- main.cpp
+++ main.cpp
@@ -1,66 +1,97 @@
 #include <iostream>
+#include <memory>
 #include "libs/AudioFile.h"
 
 using namespace std;
 
-void process(int nSamples, int nChannels, AudioFile<double>::AudioBuffer &input) {
-    for (int i = 1; i < nSamples; i++) {
-        for (int channel = 0; channel < nChannels; channel++) {
-            input[channel][i] = 0.5 * (input[channel][i] + input[channel][i-1]);
+int CUTOFF_FREQ = 500.0;
+int FILTER_ORD  = 4;
+
+void butterworth(int nSamples, AudioFile<double>::AudioBuffer& buffer) {
+    for (auto& channel : buffer) {
+        for (int i = 1; i < nSamples; i++) {
+            channel[i] = 0.5 * channel[i] + channel[i-1];
         }
     }
     return;
 }
 
-int main(int argc, char** argv)
-{
-    cout << "I will be a filter soon!" << endl;
-    if (argc != 2) {
-        cout << "Usage: specify a .wav file and I will filter it, outputting .old.wav" << endl;
-        return 1;
+int errorMsg() {
+    cout << "Usage: specify a .wav file and I will filter it, outputting .old.wav" << endl;
+    return 1;
+}
+
+// Presumes that the input length is at least 4.
+string suffix(string fileName) {
+    int fLast = fileName.length() - 1;
+    return fileName.substr(fLast - 3, 4);
+}
+
+// Presumes that the input length is at least 4.
+string prefix(string fileName) {
+    return fileName.substr(0, fileName.length() - 4);
+}
+
+void copyFromFile(AudioFile<double>& fileObj, string& fileLoc, AudioFile<double>::AudioBuffer& buffer) {
+    fileObj.load(fileLoc);
+
+    // format the buffer
+    int nChannels = fileObj.getNumChannels();
+    int nSamples  = fileObj.getNumSamplesPerChannel();
+    buffer.resize(nChannels);
+    for (auto& channel : buffer) {
+        channel.resize(nSamples);
     }
 
-    std::string f = argv[1];
-    if (f.length() < 4) {
-        cout << "Usage: specify a .wav file and I will filter it, outputting .old.wav" << endl;
-        return 1;
-    }
-
-    int fLast = f.length() - 1;
-    std::string suffix = f.substr(fLast - 3, 4);
-    std::string prefix = f.substr(0, f.length() - 4);
-    std::string finalName = prefix + ".new.wav";
-    if (suffix != ".wav") {
-        cout << "Usage: specify a .wav file and I will filter it, outputting .old.wav" << endl;
-        return 1;
-    }
-
-    AudioFile<double> audioFile;
-    audioFile.load(f);
-
-    AudioFile<double>::AudioBuffer finalAudio;
-    int nChannels = audioFile.getNumChannels();
-    int nSamples  = audioFile.getNumSamplesPerChannel();
-    finalAudio.resize(nChannels);
-    for (int i = 0; i < nChannels; i++) {
-        finalAudio[i].resize(nSamples);
-    }
-
+    // Fill the buffer
     for (int i = 0; i < nSamples; i++) {
         for (int channel = 0; channel < nChannels; channel++) {
-            finalAudio[channel][i] = audioFile.samples[channel][i];
+            buffer[channel][i] = fileObj.samples[channel][i];
         }
     }
+    return;
+}
 
-    process(nSamples, nChannels, finalAudio);
 
-    bool ok = audioFile.setAudioBuffer(finalAudio);
+int main(int argc, char** argv)
+{
+    cout << "I am the simplest filter!" << endl;
+    if (argc != 2)
+        return errorMsg();
 
+    string inputName = argv[1];
+    if (inputName.length() < 4)
+        return errorMsg();
+    if (suffix(inputName) != ".wav")
+        return errorMsg();
+
+    string outputName = prefix(inputName) + ".new.wav";
+    AudioFile<double> fileInterface;
+
+    // Initialize the buffer
+    //auto buffer = make_shared<AudioFile<double>::AudioBuffer>;
+    AudioFile<double>::AudioBuffer* buffer;
+    buffer = new AudioFile<double>::AudioBuffer;
+    copyFromFile(fileInterface, inputName, *buffer);
+
+    // Run each channel through a Butterworth filter
+    butterworth
+    (
+        fileInterface.getNumSamplesPerChannel(),
+        *buffer
+    );
+
+    // Finish our work with the buffer
+    bool ok = fileInterface.setAudioBuffer(*buffer);
+    delete buffer;
+    // Time to terminate the program
     if (ok) {
-        audioFile.save(finalName, AudioFileFormat::Wave);
+        fileInterface.save(outputName, AudioFileFormat::Wave);
+        cout << "Complete!";
         return 0;
     }
     else {
+        cerr << "Something went wrong when I tried to save the audio buffer!";
         return 1;
     }
 }
